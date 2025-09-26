c94175d2aee0e47106c3ef6e7f45c76956bdddd4
Ahsan <sbaseer25@gmail.com>
2025-09-26 13:16:44 -0400

    Payroll Calculator Completed

--- src/main/java/com/pluralsight/BasicCalculator.java
+++ src/main/java/com/pluralsight/BasicCalculator.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class BasicCalculator {
     public static void main(String[] args) {
         Scanner myScanner = new Scanner(System.in);
         double num1;
