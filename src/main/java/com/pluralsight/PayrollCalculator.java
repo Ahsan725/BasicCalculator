6d7e7e3ff247ae06dcf25e8a69ca067802ff1ba9
Ahsan <sbaseer25@gmail.com>
2025-09-26 13:06:38 -0400

    Payroll Calculator Completed

--- src/main/java/com/pluralsight/PayrollCalculator.java
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -1,7 +1,29 @@
 package com.pluralsight;
+import java.util.Scanner;
 
 public class PayrollCalculator {
     public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        String name;
+        int hours;
+        double payRate;
+        int overtimeHours = 0;
+        double grossPay;
 
+        System.out.println("Enter your name: ");
+        name = myScanner.nextLine();
+        System.out.println("How many hours did you work?");
+        hours = myScanner.nextInt();
+        if (hours > 40){
+            overtimeHours = hours - 40;
+            hours =  hours - overtimeHours;
+        }
+        System.out.println("What is your pay rate?");
+        payRate = myScanner.nextDouble();
+
+        grossPay = payRate * hours + (overtimeHours * (1.5 * payRate));
+
+        System.out.println("Name: " + name);
+        System.out.println("Your Gross Pay: " + grossPay);
     }
 }
